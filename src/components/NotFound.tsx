aef4d19fa02e386bf737f121704787fdb65e3c11
Ryan Bradley <ryanbradley315@gmail.com>
2023-05-18 19:44:48 -0400

    Update root div styling to address and fix overscroll issue on mobile, add better UI to '404' page.

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -3,8 +3,11 @@ import React from 'react'
 export default function NotFound() {
     return (
         <main>
-            <div className='wrapper'>
-                NotFound
+            <div className='wrapper' style={{ display: 'grid', placeItems: 'center', textAlign: 'center' }}>
+                <div>
+                    <p>The page you're looking for can't be found.</p>
+                    <p>Redirecting you to home page.</p>
+                </div>
             </div>
         </main>
     )
