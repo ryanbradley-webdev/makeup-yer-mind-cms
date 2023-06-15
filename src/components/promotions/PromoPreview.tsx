a049646032e782bb5520056e11f999726354f06b
Ryan Bradley <ryanbradley315@gmail.com>
2023-06-15 16:46:36 -0500

    Add content and styling for promotion preview.

--- src/components/promotions/PromoPreview.tsx
+++ src/components/promotions/PromoPreview.tsx
@@ -1,13 +1,47 @@
-import { Dispatch, SetStateAction } from 'react'
+import styles from './promotions.module.css'
 
 export default function PromoPreview({
     article,
     togglePreview
 }: {
     article: Promo,
-    togglePreview: Dispatch<SetStateAction<boolean>>
+    togglePreview: () => void
 }) {
+    const {
+        image,
+        title,
+        description,
+        link
+    } = article
+
     return (
-        <div>PromoPreview</div>
+
+        <div className='preview-container'>
+            
+            <button onClick={togglePreview}>&times;</button>
+
+            <div className="preview-content">
+
+                <section className={styles.promo}>
+
+                    <h1>
+                        {title}
+                    </h1>
+
+                    <img src={image} height={320} width={480} alt='' />
+
+                    <p>
+                        {description}
+                    </p>
+
+                    <p>
+                        To get started, just click <a href={link}>this link</a>!
+                    </p>
+
+                </section>
+
+            </div>
+
+        </div>
     )
 }
